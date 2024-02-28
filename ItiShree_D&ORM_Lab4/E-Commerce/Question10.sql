db481946ae774641f439b265ad30d46257fe5bdf
Iti Shree <103059786+ItiShree41@users.noreply.github.com>
2024-02-28 23:47:36 +0530

    Update Question10.sql

--- ItiShree_D&ORM_Lab4/E-Commerce/Question10.sql
+++ ItiShree_D&ORM_Lab4/E-Commerce/Question10.sql
@@ -11,7 +11,7 @@ BEGIN
 		when r.rat_ratstars>2 then "Average service"
 		else "Poor service"
 	END as type_of_service
-	from supplier as s,rating as r,`order` as o,supplier_pricing as sp
+	from supplier as s,rating as r,orders as o,supplier_pricing as sp
 	where r.ord_id=o.ord_id and sp.pricing_id=o.pricing_id and sp.supp_id=s.supp_id
 	group by supp_id;
 END//
